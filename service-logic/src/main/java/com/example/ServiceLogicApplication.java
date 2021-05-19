59079b1f7eab4f6b88e1ceb3761eec35af24aa6d
Arvind Srinivas <arvindsrinivas26@gmail.com>
2021-05-19 17:35:48 +0530

    service 2

---
+++ service-logic/src/main/java/com/example/ServiceLogicApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceLogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceLogicApplication.class, args);
+	}
+
+}
